879c9f17d8e54484028c78cc3c758a91c6fcc657
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-08-02 16:01:04 +0200

    creazione e collegamento ad altre 'stanze'

--- resources/views/rooms/basement.blade.php
+++ resources/views/rooms/basement.blade.php
@@ -4,15 +4,11 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>my house</title>
+        <title>my basement</title>
     </head>
     <body>
         <h1>Hello World</h1>
         <h3>This is my {{$class_place}}!</h3>
-        <a href="{{url('/rooms/basement')}}">go to basement<a>
-        <br>
-        <a href="{{url('/rooms/bedroom')}}">go to bedroom<a>
-        <br>
-        <a href="{{url('/rooms/kitchen')}}">go to kitchen<a>
+        <a href="{{url('/')}}">return to home<a>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['class_place' => 'house']);
+});
+Route::get('/rooms/basement', function () {
+    return view('rooms.basement', ['class_place' => 'basement']);
+});
+Route::get('/rooms/bedroom', function () {
+    return view('rooms.bedroom', ['class_place' => 'bedroom']);
+});
+Route::get('/rooms/kitchen', function () {
+    return view('rooms.kitchen', ['class_place' => 'kitchen']);
 });
